e2161603344f7ede16df2225ff06a9f63673ede0
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-12 15:02:26 +0530

    Add files via upload

---
+++ BP_P47.py
@@ -0,0 +1,36 @@
+# Pytho Program 
+
+# Perform Operation on Matrices
+
+# import lib.
+import numpy as np
+
+# Create first Matrix
+mat1 = np.array([[2, 5], [7, 5]])
+
+# Create Second Matrix
+mat2 = np.array([[25, 3], [9, 7]])
+
+print("Printing elements of First matrix")
+print(mat1)
+
+print("Printing elements of Second matrix")
+print(mat2)
+
+# Adding two Matrix
+print("\nAddition of Matrix is: ")
+print(np.add(mat1, mat2))
+
+# Subtraction of Matrix
+print("\nSubtraction of Matrix is: ")
+print(np.subtract(mat1, mat2))
+
+# Multiply tow matrix
+print("\nMultiplication of Matrix is: ")
+print(np.multiply(mat1, mat2))
+
+# division of two matrix
+print("\nDivision of Matrix is: ")
+print(np.divide(mat1, mat2))
+
+# Thanks for Watching
